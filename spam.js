fc69799b9f2ef9512bc803d54916a60cfc3b183f
= <alexandrsemin2033@gmail.com>
2021-07-21 16:00:11 +0300

    small improves

--- spam.js
+++ spam.js
@@ -1,29 +1,66 @@
-const config = require("./config.js")
-const { VK } = require("vk-io")
+const { VK } = require("vk-io");
+
+const config = require("./config.js");
+
 const vk = new VK({
-	token: config.token
-})
-if(config.invite_id < 1 || isNaN(config.invite_id))return console.error('некорректно введён параметр "invite_id"')
-if(config.amount < 1 || isNaN(config.amount))return console.error('некоррктно введён параметр "amount"')
-let c = 0
-setInterval(function() {
-	c += 1
- 	vk.api.messages.removeChatUser({ // удаляет пользователя из беседы
-		chat_id: config.chat_id,
-		user_id: config.invite_id
-	}).catch(function(){
-		return console.error('облом.')
-	})
-	vk.api.messages.addChatUser({ // приглашает пользователя в беседу
-		chat_id: config.chat_id,
-		user_id: config.invite_id
-	}).catch(function(){
-		return console.error('облом.')
-	})
-	
-	console.log(`Заспамлено уже: ${c} раз`) // каунтер
-	if(c == config.amount){
-		console.log('готово.')
-		break
+	token: config.token,
+});
+let inviteCount = 0;
+let errorsCount = 0;
+
+if (config.invite_id < 1 || isNaN(config.invite_id)) {
+	console.error('Некорректно введён параметр "invite_id"');
+	process.exit();
+}
+if (config.amount < 1 || isNaN(config.amount)) {
+	console.error('Некоррктно введён параметр "amount"');
+	process.exit();
+}
+if (config.errorLimit < 1 || isNaN(config.errorLimit)) {
+	console.error('Некоррктно введён параметр "errorLimit"');
+	process.exit();
+}
+
+const interval = setInterval(async function () {
+	let isError = false;
+	++inviteCount;
+	await vk.api.messages
+		.removeChatUser({
+			// удаляет пользователя из беседы
+			chat_id: config.chat_id,
+			user_id: config.invite_id,
+		})
+		.catch(() => {
+			console.error("Облом.");
+			++errorsCount;
+			isError = true;
+		});
+	await vk.api.messages
+		.addChatUser({
+			// приглашает пользователя в беседу
+			chat_id: config.chat_id,
+			user_id: config.invite_id,
+		})
+		.catch(() => {
+			{
+				console.error("Облом.");
+				++errorsCount;
+				isError = true;
+			}
+		});
+
+	if (isError) {
+		if (errorsCount >= config.errorLimit) {
+			console.log(`Лимит ошибок превышен.`);
+			clearInterval(interval);
+			process.exit();
+		}
+	} else {
+		console.log(`Заспамлено уже: ${c} раз`); // каунтер
+		if (c === config.amount) {
+			console.log("Готово.");
+			clearInterval(interval);
+			process.exit();
+		}
 	}
-}, config.time*1000);
+}, config.time * 1000);
